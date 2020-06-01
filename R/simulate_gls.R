b846a544391839e0c96614254674e1302b4f49b0
Fernando Miguez <fer.eze.miguez@gmail.com>
2020-06-01 11:56:09 -0500

    Adding the ability to simulate for generalized least-squares

---
+++ R/simulate_gls.R
@@ -0,0 +1,134 @@
+### FEM: I include this because it was in the original file gnls.R
+###
+### Copyright 1997-2003  Jose C. Pinheiro,
+###                      Douglas M. Bates <bates@stat.wisc.edu>
+### Copyright 2006-2015  The R Core team
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+#
+
+#' This function is based on \code{\link[nlme]{predict.gls}} function 
+#' 
+#' @title Simulate fitted values from an object of class \code{\link[nlme]{gls}}
+#' @description Simulate values from an object of class gls. Unequal variances, 
+#' as modeled using the \sQuote{weights} option are supported, and there is experimental
+#' code for dealing with the \sQuote{correlation} structure. 
+#' @name simulate_gls
+#' @param object object of class \code{\link[nlme]{gls}}
+#' @param psim parameter simulation level, 0: for fitted values, 1: for simulation from 
+#' fixed parameters (assuming a fixed vcov matrix), 2: for simulation considering the 
+#' uncertainty in the residual standard error (sigma), this returns data which
+#' will appear similar to the observed values
+#' @param na.action default \sQuote{na.fail}. See \code{\link[nlme]{predict.gls}}
+#' @param naPattern missing value pattern. See \code{\link[nlme]{predict.gls}}
+#' @param ... additional arguments (it is possible to supply a newdata this way)
+#' @return It returns a vector with simulated values with length equal to the number of rows 
+#' in the original data
+#' @details It uses function \code{\link[MASS]{mvrnorm}} to generate new values for the coefficients
+#' of the model using the Variance-Covariance matrix \code{\link{vcov}}. This variance-covariance matrix 
+#' refers to the one for the parameters 'beta', not the one for the residuals.
+#' @seealso \code{\link[nlme]{predict.gls}}
+#' @export
+#' @examples 
+#' \donttest{
+#' require(nlme)
+#' data(Orange)
+#' 
+#' fit.gls <- gls(circumference ~ age, data = Orange, 
+#'                weights = varPower())
+#' 
+#' ## Visualize covariance matrix
+#' fit.gls.vc <- var_cov(fit.gls)
+#' image(log(fit.gls.vc[,ncol(fit.gls.vc):1]))
+#' 
+#' sim <- simulate_gls(fit.gls)
+#' }
+
+simulate_gls <- function(object, psim = 1, na.action = na.fail, naPattern = NULL, ...){
+
+  if(!inherits(object, "gls")) stop("This function is only for 'gls' objects")
+  
+  ##
+  ## method for predict() designed for objects inheriting from class gls
+  ##
+  newdata <- nlme::getData(object)
+
+  form <- getCovariateFormula(object)
+  mfArgs <- list(formula = form, data = newdata, na.action = na.action)
+  mfArgs$drop.unused.levels <- TRUE
+  dataMod <- do.call(model.frame, mfArgs)
+  ## making sure factor levels are the same as in contrasts
+  contr <- object$contrasts
+  for(i in names(dataMod)) {
+    if (inherits(dataMod[,i], "factor") && !is.null(contr[[i]])) {
+      levs <- levels(dataMod[,i])
+      levsC <- dimnames(contr[[i]])[[1]]
+      if (any(wch <- is.na(match(levs, levsC)))) {
+        stop(sprintf(ngettext(sum(wch),
+                              "level %s not allowed for %s",
+                              "levels %s not allowed for %s"),
+                     paste(levs[wch], collapse = ",")),
+             domain = NA)
+      }
+      attr(dataMod[,i], "contrasts") <- contr[[i]][levs, , drop = FALSE]
+      ##      if (length(levs) < length(levsC)) {
+      ##        if (inherits(dataMod[,i], "ordered")) {
+      ##          dataMod[,i] <- ordered(as.character(dataMod[,i]), levels = levsC)
+      ##        } else {
+      ##          dataMod[,i] <- factor(as.character(dataMod[,i]), levels = levsC)
+      ##        }
+      ##      }
+    }
+  }
+  N <- nrow(dataMod)
+  if (length(all.vars(form)) > 0) {
+    ##    X <- model.matrix(form, dataMod, contr)
+    X <- model.matrix(form, dataMod)
+  } else {
+    X <- array(1, c(N, 1), list(row.names(dataMod), "(Intercept)"))
+  }
+  
+  ##----------- FEM 2020-06-01 ---------------##
+  if(psim == 0){
+    cf <- coef(object)  
+  }
+  
+  if(psim == 1){
+    cf <- MASS::mvrnorm(n = 1, mu = coef(object), Sigma = vcov(object))
+  }
+  
+  if(psim == 2){
+    ## For more details see the comments in simulate_gnls
+    cf <- MASS::mvrnorm(n = 1, mu = coef(object), Sigma = vcov(object))
+    if(is.null(object$modelStruct$corStruct)){
+      rsds.std <- stats::rnorm(N, 0, 1)
+      rsds <- rsds.std * attr(residuals(object), "std") ## This last term is 'sigma'
+    }else{
+      var.cov.err <- var_cov(object)
+      rsds <- MASS::mvrnorm(mu = residuals(object), Sigma = var.cov.err)
+    }
+  }
+  
+  val <- c(X[, names(cf), drop = FALSE] %*% cf)
+  
+  ## If psim == 2, we add residuals
+  if(psim == 2) val <- val + rsds
+  
+  lab <- "Predicted values"
+  if (!is.null(aux <- attr(object, "units")$y)) {
+    lab <- paste(lab, aux)
+  }
+  structure(val, label = lab)
+}
+
