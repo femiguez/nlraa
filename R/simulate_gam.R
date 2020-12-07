873b37ba96e0d5b813a2fb329f0cea2757ce4174
Fernando Miguez <fer.eze.miguez@gmail.com>
2020-12-07 16:34:33 -0600

    Adding functionality for simulating from gam fits

---
+++ R/simulate_gam.R
@@ -0,0 +1,162 @@
+#'
+#' This function is probably redundant. Simply using \code{\link{simulate}}
+#' generates data from the correct distribution for objects which inherit 
+#' class \code{\link[stats]{lm}}. The difference is that I'm trying to add the 
+#' uncertainty in the parameter estimates.
+#' 
+#' @title Simulate responses from a generalized additive linear model \code{\link[mgcv]{gam}}
+#' @name simulate_gam
+#' @description By sampling from the vector of coefficients it is possible to simulate
+#' data from a \sQuote{gam} model. 
+#' @param object object of class \code{\link[mgcv]{gam}} or \code{\link[stats]{glm}}.
+#' @param psim parameter simulation level (an integer, 0, 1, 2 or 3).
+#' @param nsim number of simulations to perform
+#' @param resid.type type of residual to use. \sQuote{none}, \sQuote{resample}, \sQuote{normal} or \sQuote{wild}.
+#' @param value either \sQuote{matrix} or \sQuote{data.frame}
+#' @param ... additional arguments (none used at the moment)
+#' @note psim = 3 is not implemented at the moment.
+#' @return matrix or data.frame with responses
+#' @details 
+#' These are the options that control the parameter simulation level
+#' \describe{
+#'   \item{psim = 0}{returns the fitted values}
+#'   \item{psim = 1}{simulates a beta vector (mean response)}
+#'   \item{psim = 2}{simulates observations according to the residual type (similar to observed data)}
+#'   \item{psim = 3}{simulates a beta vector, considers uncertainty in the variance covariance matrix of beta and adds residuals (prediction)}
+#'  }
+#' The residual type (resid.type) controls how the residuals are generated. 
+#' They are either resampled, simulated from a normal distribution or \sQuote{wild} where the
+#' Rademacher distribution is used (\url{https://en.wikipedia.org/wiki/Rademacher_distribution}).
+#' Resampled and normal both assume iid, but \sQuote{normal} makes the stronger assumption of normality.
+#' \sQuote{wild} does not assume constant variance, but it assumes symmetry.
+#' @seealso \code{\link{predict}}, \code{\link{simulate}} and \code{\link{simulate_lm}}.
+#' @references Generalized Additive Models. An Introduction with R. Second Edition. (2017) Simon N. Wood. CRC Press. 
+#' @export
+#' @examples
+#' \donttest{
+#' require(ggplot2)
+#' require(mgcv)
+#' ## These count data is from GAM book by Simon Wood (pg. 132) - see reference
+#' y <- c(12, 14, 33, 50, 67, 74, 123, 141, 165, 204, 253, 246, 240)
+#' t <- 1:13
+#' dat <- data.frame(y = y, t = t)
+#' fit <- gam(y ~ t + I(t^2), family = poisson, data = dat)
+#' sims <- simulate_gam(fit, nsim = 100, value = "data.frame")
+#' 
+#' ggplot(data = sims) + 
+#'   geom_line(aes(x = t, y = sim.y, group = ii), 
+#'             color = "gray", alpha = 0.5) + 
+#'   geom_point(aes(x = t, y = y)) 
+#' }
+#' 
+
+simulate_gam <- function(object, nsim = 1, psim = 1, 
+                         resid.type = c("none", "resample", "normal", "wild"),
+                         value = c("matrix", "data.frame"), ...){
+  
+  if(!inherits(object, "glm")) stop("Object should be of class 'glm' or 'gam'")
+  
+  resid.type <- match.arg(resid.type)
+  value <- match.arg(value)
+  
+  xargs <- list(...)
+  if(is.null(xargs$newdata)){
+    newdata <- NULL 
+  }else{
+    newdata <- xargs$newdata
+  } 
+
+  if(!is.null(newdata) && psim > 1)
+    stop("'newdata' is not compatible with 'psim > 1'")
+  
+  nr <- ifelse(is.null(newdata), stats::nobs(object), nrow(newdata))
+  ans.mat <- matrix(nrow = nr, ncol = nsim)  
+  
+  if(!is.null(newdata) && value == "data.frame")
+    stop("'newdata' is not compatible with 'value = data.frame'")
+  
+  ## They say that replicate is faster than this,
+  ## but not when storage is pre-allocated
+  for(i in 1:nsim){
+    ans.mat[,i] <- simulate_gam_one(object, psim = psim, resid.type = resid.type, 
+                                   newdata = newdata)  
+  }
+  
+  if(value == "matrix"){
+    colnames(ans.mat) <- paste0("sim_",1:nsim)
+    return(ans.mat)
+  }else{
+    dfr <- eval(getCall(object)$data)
+    if(is.null(dfr)) stop("data argument should be supplied")
+    ## I think this is guaranteed to exist
+    ## but it can be weird
+    ans.dat <- data.frame(ii = as.factor(rep(1:nsim, each = nrow(dfr))),
+                          dfr,
+                          sim.y = c(ans.mat), 
+                          row.names = 1:c(nsim * nr))
+    return(ans.dat)
+  }
+}
+
+simulate_gam_one <- function(object, psim = 1, 
+                             resid.type = c("none", "resample", "normal", "wild"),
+                             newdata = NULL){
+  
+  resid.type <- match.arg(resid.type)
+  
+  X <- stats::model.matrix(object)
+  n <- stats::nobs(object)
+  
+  if(!is.null(newdata)){
+    ## Check that names are correct
+    if(!identical(names(newdata), attr(object$terms, "term.labels")))
+      stop("names in 'newdata' do not correspond to 'term.labels'")
+    X <- stats::model.matrix(object, data = newdata)
+  }
+
+  if(resid.type == "resample"){
+    rsd0 <- residuals(object, type = "scaled.pearson") ## Are these  ~ N(0, 1) ?
+    rsd.sd <- sqrt(object$family$variance(fitted(object)))
+    rsds <- sample(rsd0, size = n, replace = TRUE) * rsd.sd     
+  }
+  
+  if(resid.type == "normal"){
+    rsd.sd <- sqrt(object$family$variance(fitted(object)))
+    rsds <- rnorm(n = n, sd = rsd.sd)
+  }
+  
+  if(resid.type == "wild"){
+    rsd0 <- residuals(object, type = "response")
+    rsds <- sample(c(-1, 1), size = n, replace = TRUE) * rsd0
+  }
+  
+  ## Nothing is simulated, fitted values
+  if(psim == 0){
+    betav <- stats::coef(object)
+    ans <- object$family$linkinv(X %*% betav)
+  }
+  
+  ## Simulate from coefficient vector only (beta)
+  if(psim == 1){
+    betav <- MASS::mvrnorm(mu = coef(object), Sigma = vcov(object))  
+    ans <- object$family$linkinv(X %*% betav)
+  }
+  
+  ## Simulate using default simulate.lm method
+  if(psim == 2 && resid.type == "none"){
+    ans <- simulate(object, nsim = 1)[,1]
+  }
+  
+  if(psim == 2 && resid.type != "none"){
+    betav <- MASS::mvrnorm(mu = coef(object), Sigma = vcov(object))  
+    ans <- object$family$linkinv(X %*% betav) + rsds
+    ## not sure for how many families only a discrete response makes sense
+    if(object$family$family == "poisson") ans <- round(ans)
+  }
+  
+  if(psim == 3){
+    stop("Not implemented yet")
+  }
+  
+  return(ans)
+}
---
+++ tests/test_simulate_gam.R
@@ -0,0 +1,140 @@
+# Testing simulate_gam
+require(ggplot2)
+require(nlraa)
+require(nlme)
+require(mgcv)
+
+if(Sys.info()[["user"]] == "fernandomiguez"){
+
+  y <- c(12, 14, 33, 50, 67, 74, 123, 141, 165, 204, 253, 246, 240)
+  t <- 1:13
+
+  dat <- data.frame(y = y, t = t)
+
+  ggplot(data = dat, aes(x = t, y = y)) + geom_point()
+
+  ## From page 132 form GAM book by Simon Wood
+  m1 <- gam(y ~ t + I(t^2), data = dat, family = poisson)
+
+  ggplot(data = dat, aes(x = t, y = y)) + 
+    geom_point() + 
+    geom_line(aes(y = fitted(m1)))
+
+  m1.sim <- simulate_gam(m1, nsim = 1e3)
+
+  m1.sims <- summary_simulate(m1.sim)
+  datA <- cbind(dat, m1.sims)
+
+  ggplot(data = datA, aes(x = t, y = y)) + 
+    geom_point() + 
+    geom_line(aes(y = Estimate)) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "purple", alpha = 0.3) + 
+    ggtitle("Simulate + summary_simulate method")
+
+  ## Compare to native method from mgcv package
+  m1.prd <- predict(m1, se.fit = TRUE, type = "response")
+
+  m1.prdd <- data.frame(dat, prd = m1.prd$fit, 
+                        lwr = m1.prd$fit - 1.96 * m1.prd$se.fit, 
+                        upr = m1.prd$fit + 1.96 * m1.prd$se.fit)
+
+  ggplot(data = m1.prdd, aes(x = t, y = y)) + 
+    geom_point() + 
+    geom_line(aes(y = prd)) + 
+    geom_ribbon(aes(ymin = lwr, ymax = upr), fill = "purple", alpha = 0.3) + 
+    ggtitle("Built-in predict.gam method")
+
+  ## Prediction. The default method uses 'simulate.lm' under the hood
+  m1.simP <- simulate_gam(m1, nsim = 1e3, psim = 2)
+
+  m1.simPs <- summary_simulate(m1.simP)
+  datAP <- cbind(dat, m1.simPs)
+
+  ggplot(data = datAP, aes(x = t, y = y)) + 
+    geom_point() + 
+    geom_line(aes(y = Estimate)) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "purple", alpha = 0.3)
+
+  ## This method includes the uncertainty in the coefficients and the residuals
+  m1.simP2 <- simulate_gam(m1, nsim = 1e3, psim = 2, resid.type = "resample")
+
+  m1.simP2s <- summary_simulate(m1.simP2)
+  datAP2 <- cbind(dat, m1.simP2s)
+
+  ggplot(data = datAP2, aes(x = t, y = y)) + 
+    geom_point() + 
+    geom_line(aes(y = Estimate)) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "purple", alpha = 0.3)
+
+  ## Wild residuals 
+  m1.simPW <- simulate_gam(m1, nsim = 1e3, psim = 2, resid.type = "wild")
+
+  m1.simPWs <- summary_simulate(m1.simPW)
+  datAPW <- cbind(dat, m1.simPWs)
+
+  ggplot(data = datAPW, aes(x = t, y = y)) + 
+    geom_point() + 
+    geom_line(aes(y = Estimate)) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "purple", alpha = 0.3)
+  
+  ## Dataset Soybean
+  data(Soybean)
+  
+  fms.G <- gam(weight ~ Time + s(Time), data = Soybean)
+  fms.C <- lm(weight ~ Time + I(Time^2) + I(Time^3), data = Soybean)
+  fms.B <- nls(weight ~ SSbgrp(Time, w.max, lt.e, ldt), data = Soybean)
+
+  ## AIC strongly favors GAM and BIC strongly favors beta
+  IC_tab(fms.G, fms.C, fms.B, criteria = "AIC")
+  IC_tab(fms.G, fms.C, fms.B, criteria = "BIC")
+  
+  ggplot(data = Soybean, aes(x = Time, y = weight)) + 
+    geom_point() + 
+    geom_line(aes(y = fitted(fms.C), color = "Cubic")) + 
+    geom_line(aes(y = fitted(fms.G), color = "GAM")) + 
+    geom_line(aes(y = fitted(fms.B), color = "Beta"))
+  
+  ## Prediction based on GAM
+  prd <- predict_gam(fms.G, interval = "confidence")
+  
+  SoybeanAG <- cbind(Soybean, prd)
+  
+  ggplot(data = SoybeanAG, aes(x = Time, y = weight)) + 
+    geom_point() + 
+    geom_line(aes(y = fitted(fms.G), color = "GAM")) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "purple", alpha = 0.3)
+
+  ## Prediction based on beta
+  prdc <- predict_nls(fms.B, interval = "confidence")
+  prdp <- predict_nls(fms.B, interval = "prediction")
+  colnames(prdp) <- paste0("p", c("Estimate", "Est.Error", "Q2.5", "Q97.5"))
+  
+  SoybeanAB <- cbind(Soybean, prdc, prdp)
+  
+  ggplot(data = SoybeanAB, aes(x = Time, y = weight)) + 
+    geom_point() + 
+    geom_line(aes(y = fitted(fms.B), color = "beta")) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5), fill = "purple", alpha = 0.3) + 
+    geom_ribbon(aes(ymin = pQ2.5, ymax = pQ97.5), fill = "purple", alpha = 0.2)
+  
+  ## Clearly a gnls model would be better
+  fms.Bg <- gnls(weight ~ SSbgrp(Time, w.max, lt.e, ldt), data = Soybean,
+                 weights = varPower())
+
+  IC_tab(fms.G, fms.C, fms.B, fms.Bg, criteria = "AIC")    
+  
+  prdc.bg <- predict_nlme(fms.Bg, interval = "confidence")
+  prdp.bg <- predict_nlme(fms.Bg, interval = "prediction")  
+  colnames(prdp.bg) <- paste0("p", c("Estimate", "Est.Error", "Q2.5", "Q97.5"))
+
+  SoybeanBG <- cbind(Soybean, prdc.bg, prdp.bg)
+  
+  ggplot(data = SoybeanBG, aes(x = Time, y = weight)) + 
+    geom_point() + 
+    geom_line(aes(y = fitted(fms.Bg), color = "mean")) + 
+    geom_ribbon(aes(ymin = Q2.5, ymax = Q97.5, color = "confidence"), fill = "purple", alpha = 0.3) + 
+    geom_ribbon(aes(ymin = pQ2.5, ymax = pQ97.5, color = "prediction"), fill = "purple", alpha = 0.2) + 
+    ggtitle("Beta growth with increasing variance is the best model")
+  
+}
+
